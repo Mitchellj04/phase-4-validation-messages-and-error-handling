6ad0da3a22ff1cdda43685286bf25c081bd8187d
JustinMitchell <mitchelljm04@gmail.com>
2022-12-08 13:46:40 -0500

    commit tree 874a6566a44cfa05e0b41eb72fc35997af0d1ab1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :movie_create_error
   wrap_parameters false
   
   def index
@@ -7,11 +8,15 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
   end
 
   private
+
+  def movie_create_error invalid
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
 
   def movie_params
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
